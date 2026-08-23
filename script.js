3385f8bb92ea365c4788609b4af683e859b7f799
Mohammad Juned <mohammad.juned.dev@gmail.com>
2026-08-23 22:57:19 +0530

    for the game the interface is done

---
+++ script.js
@@ -0,0 +1,31 @@
+const board = document.querySelector(".board");
+const blockHeight = 30;
+const blockWidth = 30;
+
+const cols = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
+const blocks = [];
+const snake = [
+  { x: 1, y: 3 },
+  { x: 1, y: 4 },
+  { x: 1, y: 5 },
+];
+
+
+
+for (let row = 0; row < rows; row++) {
+  for (let col = 0; col < cols; col++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    board.appendChild(block);
+    block.innerText = `${row}-${col}`;
+    blocks[`${row}-${col}`] = block;
+  }
+}
+
+function render(){
+    snake.forEach((segment)=>{
+console.log(segment);
+
+    })
+}
